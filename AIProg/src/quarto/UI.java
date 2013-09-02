976ec90ecd62083163f135f14877156805daec28
simonra <simon.randby+github@gmail.com>
2013-09-02 13:18:39 +0200

    La til lese input i UI

--- AIProg/src/quarto/UI.java
+++ AIProg/src/quarto/UI.java
@@ -1,5 +1,25 @@
 package quarto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class UI {
+	
+	/**Leser input fra konsollen*/
+	public String readInput(){
+		String s = "";
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			s = br.readLine();
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return s;
+	}
+	
+	
+	
 	
 }
