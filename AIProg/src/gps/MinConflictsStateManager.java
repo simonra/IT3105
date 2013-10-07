18c7b61458aca6806f6a1d5442af490419b51870
Simon Randby <simon.randby+github@gmail.com>
2013-10-07 13:29:57 +0200

    la inn toString i mcStateManager. (Burde kanskje lage en super-state manager som forcer implementeringen av det? Problemet er at det ikke er noe annet fornuftig å ha i en slik super-greie...)

---
+++ AIProg/src/gps/MinConflictsStateManager.java
@@ -0,0 +1,23 @@
+package gps;
+
+public interface MinConflictsStateManager {
+
+	/** Calculate and return the number of conflicts in the current state */
+	int getConflicts();
+
+	/** The number of positions this mc-puzzle operates with */
+	int getPositions();
+
+	/**
+	 * The conflict vale for the state if the element on pos0 and pos1 are
+	 * swapped. Should not alter the state/revert the state to what it
+	 * originally was if it needs to be altered in the process.
+	 */
+	int getConflictValueForSwap(int pos0, int pos1);
+
+	/** Swaps the elements in pos0 and pos1. */
+	void swap(int pos0, int pos1);
+	
+	/**Should be customized to generate the string that best represents the implemented mc-state*/
+	String toString();
+}
