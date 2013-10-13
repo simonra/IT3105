ef5256818adead99f748c4e86b30690f4b1546ad
Håkon Hesselberg <hakon.hesselberg@gmail.com>
2013-10-14 01:43:41 +0200

    Not much, kommentarer

--- AIProg/src/gps/SAGCStateManager.java
+++ AIProg/src/gps/SAGCStateManager.java
@@ -4,15 +4,28 @@ import java.util.ArrayList;
 
 public class SAGCStateManager implements SAStateManager {
 
+	/** The name says it all */
+	boolean[][] neighbourMatrix;
+	/**
+	 * The int color value of the node of the index value (ex: colorOfNode[nodes
+	 * number/id] = nodes color value)
+	 */
+	int[] colorOfNodeN;
+
+	/** Instansierer matrisene, tar inn filer, lager matrisestuff */
+	public SAGCStateManager() {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public double objectiveValue() {
+		// Regne totalt antall konflikter
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getTargetObjectiveFunctionValue() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
