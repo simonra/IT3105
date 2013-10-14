923f0cb22f05c66289af5d7f767f4ee18322f8e7
Håkon Hesselberg <hakon.hesselberg@gmail.com>
2013-10-14 03:49:24 +0200

    Lagt til sudoku

---
+++ AIProg/src/gps/SASudokuStateManager.java
@@ -0,0 +1,25 @@
+package gps;
+
+import java.util.ArrayList;
+
+public class SASudokuStateManager implements SAStateManager {
+
+	@Override
+	public double objectiveValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getTargetObjectiveFunctionValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<SAStateManager> getNeighbors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
