df35a3719f5834bb1fa17f97c7c66100a96c42ca
Håkon Hesselberg <hakon.hesselberg@gmail.com>
2013-10-14 02:03:23 +0200

    SAGC state manager

--- AIProg/src/gps/SAGCStateManager.java
+++ AIProg/src/gps/SAGCStateManager.java
@@ -3,9 +3,10 @@ package gps;
 import java.util.ArrayList;
 
 public class SAGCStateManager implements SAStateManager {
-
+	public GCFileReader fileReader = new GCFileReader();
 	/** The name says it all */
 	boolean[][] neighbourMatrix;
+
 	/**
 	 * The int color value of the node of the index value (ex: colorOfNode[nodes
 	 * number/id] = nodes color value)
@@ -19,9 +20,15 @@ public class SAGCStateManager implements SAStateManager {
 
 	@Override
 	public double objectiveValue() {
-		// Regne totalt antall konflikter
-		// TODO Auto-generated method stub
-		return 0;
+		int conflicts = 0;
+		//
+		// for (int i = 0; i < k; i++) {
+		// for (int j = i + 1; j < k; j++) {
+		// if (/*replace with sammenligning av nabomatrise + farger*/)
+		// conflicts++;
+		// }
+		// }
+		return 0 - conflicts;
 	}
 
 	@Override
