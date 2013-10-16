dc16c3ebd4fd41dbed9cd0594450a4af32781e0e
Simon Randby <simon.randby+github@gmail.com>
2013-10-16 13:50:29 +0200

    La inn klasser

---
+++ AIProg/src/gps/SAKSStateManager.java
@@ -0,0 +1,27 @@
+package gps;
+
+import java.util.ArrayList;
+
+public class SAKSStateManager implements SAStateManager {
+
+	
+	
+	@Override
+	public double objectiveValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getTargetObjectiveFunctionValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<SAStateManager> getNeighbors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
