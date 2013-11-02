f26a86b9576799dca8224901e5382071653a0cce
Håkon Hesselberg <hakon.hesselberg@gmail.com>
2013-11-02 17:54:06 +0100

    a

--- AIProg/src/pso/PsoMain.java
+++ AIProg/src/pso/PsoMain.java
@@ -1,13 +1,9 @@
 package pso;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 
 public class PsoMain {
-	
-	
-	
+
 	public static void main(String[] args) {
 		Particle[] particles = new Particle[Constants.NUMBEROFPARTICLES];
 		Random random = new Random();
@@ -22,26 +18,34 @@ public class PsoMain {
 		}
 		averageFitness /= Constants.NUMBEROFPARTICLES;
 		System.out.println(averageFitness);
-		
+
 		boolean conditions = true;
 		conditions &= averageFitness < Constants.GLOBALFITNESSGOAL;
-		conditions &= counter > Constants.MAXITERATIONS;			
-		while(conditions){
+		conditions &= counter > Constants.MAXITERATIONS;
+		while (conditions) {
 			averageFitness = 0;
 			for (Particle particle : particles) {
-				particle.updateVelocity(bestSeenPosition, random.nextDouble(), random.nextDouble());
+				particle.updateVelocity(bestSeenPosition, random.nextDouble(),
+						random.nextDouble());
 				particle.updatePosition();
 				particle.evaluateFitness();
-				if(particle.fitness < particle.bestFitnessKnownToMe)
-//					Collections.copy(particle.bestPositionKnownToMe, particle.positions);
-				
+				if (particle.fitness < particle.bestFitnessKnownToMe)
+					System.arraycopy(particle.positions, 0,
+							particle.bestPositionKnownToMe, 0,
+							Constants.DIMENSIONS);
+
 			}
-			
+
 			counter++;
 			conditions &= averageFitness < Constants.GLOBALFITNESSGOAL;
-			conditions &= counter > Constants.MAXITERATIONS;			
+			conditions &= counter > Constants.MAXITERATIONS;
+
+			if (!conditions || counter % 10 == 0) {
+				System.out
+						.println("This is iteration " + counter
+								+ " and the average fitness is "
+								+ averageFitness + ".");
+			}
 		}
 	}
-	
-
 }
