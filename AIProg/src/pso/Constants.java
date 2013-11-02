eff162d8b36eff65f43035cdc7c08395c370d789
Simon Randby <simon.randby+github@gmail.com>
2013-11-03 00:30:57 +0100

    Knapsack file reader

--- AIProg/src/pso/Constants.java
+++ AIProg/src/pso/Constants.java
@@ -7,9 +7,11 @@ public interface Constants {
 	int MAXITERATIONS = 1000;
 	int NCLOSESTNEIGHBOURS = 3;
 	int TXTLINES = 2001;
+	int KNAPSACKMAXVOLUMEPERITEM = 4;
 	double GLOBALFITNESSGOAL = 0.001;
 	double C1 = 1; // Should be in the range [0,2>
 	double C2 = 1; // Should be in the range [0,2>
 	double INERTIA = 1;
 	double CIRCLEPROBLEMRANGE = 1234;
+	String KNAPSACKURL = "src/pso/pso-packages.txt";
 }
---
+++ AIProg/src/pso/KnapsackFileReader.java
@@ -0,0 +1,62 @@
+package pso;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Random;
+
+public class KnapsackFileReader {
+	
+	public static String readFile(String fileUrl) {
+		String everyThing = "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileUrl));
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				sb.append('\n');
+				line = br.readLine();
+			}
+			everyThing = sb.toString();
+			br.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return everyThing;
+	}
+	
+	
+	public static Double[][] standardKnapsack(String ksText, Random r){
+		Double[][] solutionSpace = new Double[Constants.KNAPSACKSIZE][3];
+		String[] itemLines = ksText.split("\n");
+		for (int i = 0; i < itemLines.length; i++) {
+			solutionSpace[i][0] = Double.parseDouble(itemLines[i].split(",")[0]);
+			solutionSpace[i][1] = Double.parseDouble(itemLines[i].split(",")[1]);
+			solutionSpace[i][2] = 1 + Constants.KNAPSACKMAXVOLUMEPERITEM * r.nextDouble();
+		}
+		
+		return solutionSpace;
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
